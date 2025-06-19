9a20df42a64318df56d6b8d88e63f32aabab9f7d
mtuannguyen39 <madserth0309@gmail.com>
2025-06-20 01:13:47 +0700

    22: Reviews

--- src/modules/reviews/types.ts
+++ src/modules/reviews/types.ts
@@ -1,5 +1,5 @@
 import { inferRouterOutputs } from "@trpc/server";
 import type { AppRouter } from "@/trpc/routers/_app";
 
-export type CategoriesGetManyOutput =
-  inferRouterOutputs<AppRouter>["categories"]["getMany"];
+export type ReviewsGetOneOutput =
+  inferRouterOutputs<AppRouter>["reviews"]["getOne"];
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,6 +5,7 @@ import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { checkoutRouter } from "@/modules/checkout/server/procedures";
 import { libraryRouter } from "@/modules/library/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedures";
+import { reviewsRouter } from "@/modules/reviews/server/procedures";
 import { tagsRouter } from "@/modules/tags/server/procedures";
 import { tenantsRouter } from "@/modules/tenants/server/procedures";
 
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
   tenants: tenantsRouter,
+  reviews: reviewsRouter,
   library: libraryRouter,
   checkout: checkoutRouter,
   products: productsRouter,
