f1bde41430a3f468887a5466e702f1abc92c4bc5
mtuannguyen39 <madserth0309@gmail.com>
2025-06-24 16:23:54 +0700

    Fix: resolve invalid cookie settings

--- src/modules/auth/utils.ts
+++ src/modules/auth/utils.ts
@@ -13,8 +13,12 @@ export const generateAuthCookie = async ({ prefix, value }: Props) => {
     value: value,
     httpOnly: true,
     path: "/",
-    sameSite: "none",
-    domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
-    secure: process.env.NODE_ENV === "production",
+    // Điều này có nghĩa là auth cookies sẽ được enables ở localhost
+    // Nhưng nó sẽ không hoạt động khi subdomain được bật
+    ...(process.env.NODE_ENV !== "development" && {
+      sameSite: "none",
+      domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
+      secure: process.env.NODE_ENV === "production",
+    }),
   });
 };
